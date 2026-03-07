b0e5e51fae0e67f94f3a427ed1114761240d80a2
Antony Najarro <anajarrom@miumg.edu.gt>
2026-03-07 09:33:01 -0600

    Ejercicio No. 1

--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
 package EjerciciosParcial;
 
+import java.util.Scanner;
+
 public class Main {
+	 static Scanner Sc = new Scanner(System.in);
+	 
 	public static void main(String[]args) {
-	System.out.println("Esto es una preuba de commit");
+		
+	}
+	//EjercicioNo.1
+	static void tablaDeMultiplicar() {
+		 
+		System.out.println("Ingrese un numero:(1-12)");
+		int numero = Sc.nextInt();
+	
+		for(int i = 1; i <= 12; i++ ) 
+			System.out.println(numero + " x " + i + " = " + (numero * i));
 	}
 }
